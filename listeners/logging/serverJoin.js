220a16daaf555ba02c768d08d330d1cb9967b15f
yaya <ashleelynn323@gmail.com>
2024-10-20 18:01:07 -0400

    welcome message

--- listeners/logging/serverJoin.js
+++ listeners/logging/serverJoin.js
@@ -1,5 +1,5 @@
 const { Listener } = require("gcommands");
-const { EmbedBuilder } = require("discord.js");
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 
 // Listener to check if user joined the server and send a welcome message with png attachment
 new Listener({
@@ -12,16 +12,33 @@ new Listener({
 			const welcomeChannel = await ctx.guild.channels.fetch('1269434806874411089')
 
 			const joinEmbed = new EmbedBuilder()
-				.setTitle(`<@${ctx.user.id}> has booted up ${ctx.guild.name}!`)
-				.setDescription(``)
+				.setTitle(`<:xtriangle_medium:1276262944836947999> <@${ctx.user.id}> 𝚑𝚊𝚜 𝚋𝚘𝚘𝚝𝚎𝚍 𝚞𝚙 ${ctx.guild.name}!`)
+				.setDescription(`«════✧ ✦ ✧ ✦ ✧════»`)
 				.setColor(0x8269c2)
-				.setThumbnail(`https://i.ibb.co/jDCnxc2/image-20-scaled-37x-pngcrushed.png`)
-				.setImage(``)
-				.setTimestamp();
+				.setThumbnail(ctx.displayAvatarURL())
+				.setImage('https://i.ibb.co/GQjsbtK/image-19-scaled-14x-pngcrushed.png')
+				.setTimestamp()
+				.setFooter({
+					text: `${ctx.guild.name} • Members: ${ctx.guild.memberCount}`, // Footer text
+					iconURL: ctx.guild.iconURL() // Optional: Server icon URL
+				})
+
+				const row = new ActionRowBuilder()
+				.addComponents(
+					new ButtonBuilder()
+						.setLabel('<:xtriangle_small:1276263767872770108> Write your intro!')
+						.setStyle(ButtonStyle.Link)
+						.setURL('https://discord.com/channels/1269419817811709952/1269445235239424010'),
+					new ButtonBuilder()
+						.setLabel('<:xtriangle_small:1276263767872770108> Get colors!')
+						.setStyle(ButtonStyle.Link)
+						.setURL('https://discord.com/channels/1269419817811709952/1269445113227251765'), 
+				);
 
 			await welcomeChannel.send({
-				content: `<:xannounce:1276188470250832014> <@&1270212130204811376> <:xannounce:1276188470250832014>`,
+				content: `<:xannounce:1276188470250832014> <@&1270212130204811376> welcome <@${ctx.user.id}> <:xannounce:1276188470250832014>`,
 				embeds: [joinEmbed],
+				components: [row],
 			});
 		} catch (error) {
 			console.log(error);
