6d45a1cdc8ab9dd19ba6a0e88f5d2afba9d63ac5
Ashlynn <ashleelynn323@gmail.com>
2024-11-07 19:12:24 -0500

    case delete

---
+++ commands/slash/moderation/deleteCase.js
@@ -0,0 +1,53 @@
+const { Command, CommandType, Argument, ArgumentType } = require('gcommands');
+const { EmbedBuilder } = require('discord.js');
+const caseSchema = require('../../../schemas/caseSchema');
+
+new Command({
+  name: 'view-cases',
+  description: 'Moderation: Cases',
+  type: [CommandType.SLASH],
+  arguments: [
+    new Argument({
+      name: 'case',
+      description: 'If looking for specific case',
+      type: ArgumentType.STRING,
+      required: true,
+    }),
+  ],
+
+
+  run: async (ctx) => {
+    const { guild, client } = ctx;
+    await ctx.interaction.deferReply();
+    const moderatorRole = ctx.guild.roles.cache.get('1269757597301604423');
+    if (!moderatorRole || !ctx.member.roles.cache.has(moderatorRole.id)) {
+      return ctx.reply({ content: "You do not have permission to use this command.", ephemeral: true });
+    }
+
+    const specCase = ctx.arguments.getString('case');
+
+      if (specCase) {
+        const caseData = await caseSchema.find({ Guild: guild.id, _id: specCase })
+
+        const caseEmbed = new EmbedBuilder()
+        .setTitle(`Case Deletion`)
+        .setColor('red')
+        .setFooter({
+          text: `${ctx.guild.name}`, // Footer text
+          iconURL: ctx.guild.iconURL()
+        })
+        .setTimestamp();
+
+      // If no leaderboard, inform user.
+      if (!caseData.length) {
+        caseEmbed.setDescription('There is not a case with that ID.');
+      } else {
+        caseEmbed.setDescription(`Case ${specCase} deleted`)
+      }
+
+      await caseSchema.deleteOne({ Guild: guild.id, _id: specCase });
+      // Send the leaderboard embed
+      await ctx.interaction.editReply({ embeds: [caseEmbed] });
+   } 
+  }
+})
