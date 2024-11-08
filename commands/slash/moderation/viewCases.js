2d242070c5019b1f5fc125843be7d5d9422fdd0e
Ashlynn <ashleelynn323@gmail.com>
2024-11-07 19:02:36 -0500

    viewcases

--- commands/slash/moderation/viewCases.js
+++ commands/slash/moderation/viewCases.js
@@ -37,45 +37,195 @@ new Command({
     const specCase = ctx.arguments.getString('case');
 
    if (sortType === null) {
-            // Get top 10 docs in xpData based on XP, Level fields
-            const xpData = await levelSchema.find({ Guild: guild.id })
-            .sort({
-              XP: -1,
-              Level: -1,
-            })
-            .limit(10);
-  
-          // Create an embed to send the leaderboard
-          const xpEmbed = new EmbedBuilder()
-            .setTitle(`<:announce:1276188470250832014> 𝚇𝙿 𝙻𝙴𝙰𝙳𝙴𝚁𝙱𝙾𝙰𝚁𝙳 <:announce:1276188470250832014>`)
-            .setColor(0x8269c2)
-            .setFooter({
-              text: `${ctx.guild.name} • Members: ${ctx.guild.memberCount}`, // Footer text
-              iconURL: ctx.guild.iconURL()
-            })
-            .setTimestamp();
-  
-          // If no leaderboard, inform user.
-          if (!xpData.length) {
-            xpEmbed.setDescription('This server does not have a leaderboard yet!');
-          } else {
-            // Array consisting of name/level/xp details (string) for each user
-            const userDescriptions = [];
-  
-            // Iterates through each xpData document to create name/level/xp details for user and pushes them to
-            // to userDescriptions array
-            for (xpDataDoc of xpData) {
-              const { User, XP, Level } = xpDataDoc;
-              const member = await client.users.fetch(User);
-              userDescriptions.push(`${xpData.indexOf(xpDataDoc) + 1}. ${member.tag} | XP: ${XP} | Level: ${Level}`);
-            };
-  
-            // Join elements in user descriptions array with \n (new line)
-            xpEmbed.setDescription(`\`\`\`${userDescriptions.join('\n')}\`\`\``);
-          }
-  
-          // Send the leaderboard embed
-          await ctx.interaction.editReply({ embeds: [xpEmbed] });
+        // Get top 10 docs in xpData based on XP, Level fields
+
+      if (specCase) {
+        const caseData = await caseSchema.find({ Guild: guild.id, _id: specCase })
+
+        const caseEmbed = new EmbedBuilder()
+        .setTitle(`Moderation: Cases (Sort: Time)`)
+        .setColor(0x8269c2)
+        .setFooter({
+          text: `${ctx.guild.name}`, // Footer text
+          iconURL: ctx.guild.iconURL()
+        })
+        .setTimestamp();
+
+      // If no leaderboard, inform user.
+      if (!caseData.length) {
+        caseEmbed.setDescription('There are no cases to show.');
+      } else {
+        for (caseDataDoc of caseData) {
+          const { _id, User, Type, Moderator } = caseDataDoc;
+          const member = await client.users.fetch(User);
+          const mod = await client.users.fetch(Moderator);
+          caseEmbed.setDescription(`Case: ${_id} | User: ${member.tag} | Type: ${Type} | Moderator: ${mod.tag}`);
+        };
+      }
+
+      // Send the leaderboard embed
+      await ctx.interaction.editReply({ embeds: [caseEmbed] });
+      } else {
+
+      const caseData = await caseSchema.find({ Guild: guild.id })
+      .sort({
+        Time: -1,
+      });
+
+      // Create an embed to send the leaderboard
+      const caseEmbed = new EmbedBuilder()
+        .setTitle(`Moderation: Cases (Sort: Time)`)
+        .setColor(0x8269c2)
+        .setFooter({
+          text: `${ctx.guild.name}`, // Footer text
+          iconURL: ctx.guild.iconURL()
+        })
+        .setTimestamp();
+
+      // If no leaderboard, inform user.
+      if (!caseData.length) {
+        caseEmbed.setDescription('There are no cases to show.');
+      } else {
+        // Array consisting of name/level/xp details (string) for each user
+        const caseDescriptions = [];
+
+        // Iterates through each xpData document to create name/level/xp details for user and pushes them to
+        // to userDescriptions array
+        for (caseDataDoc of caseData) {
+          const { _id, User, Type, Moderator } = caseDataDoc;
+          const member = await client.users.fetch(User);
+          const mod = await client.users.fetch(Moderator);
+          caseDescriptions.push(`Case: ${_id} | User: ${member.tag} | Type: ${Type} | Moderator: ${mod.tag}`);
+        };
+
+        // Join elements in user descriptions array with \n (new line)
+        caseEmbed.setDescription(`\`\`\`${caseDescriptions.join('\n')}\`\`\``);
+      }
+
+      // Send the leaderboard embed
+      await ctx.interaction.editReply({ embeds: [caseEmbed] });
+    }
    } 
+
+   if (sortType === 'user') {
+          // Get top 10 docs in xpData based on XP, Level fields
+      const caseData = await caseSchema.find({ Guild: guild.id })
+      .sort({
+        User: -1,
+      });
+
+      // Create an embed to send the leaderboard
+      const caseEmbed = new EmbedBuilder()
+        .setTitle(`Moderation: Cases (Sort: User)`)
+        .setColor(0x8269c2)
+        .setFooter({
+          text: `${ctx.guild.name}`, // Footer text
+          iconURL: ctx.guild.iconURL()
+        })
+        .setTimestamp();
+
+      // If no leaderboard, inform user.
+      if (!caseData.length) {
+        caseEmbed.setDescription('There are no cases to show.');
+      } else {
+        // Array consisting of name/level/xp details (string) for each user
+        const caseDescriptions = [];
+
+        // Iterates through each xpData document to create name/level/xp details for user and pushes them to
+        // to userDescriptions array
+        for (caseDataDoc of caseData) {
+          const { _id, User, Type, Moderator } = caseDataDoc;
+          const member = await client.users.fetch(User);
+          const mod = await client.users.fetch(Moderator);
+          caseDescriptions.push(`Case: ${_id} | User: ${member.tag} | Type: ${Type} | Moderator: ${mod.tag}`);
+        };
+
+        // Join elements in user descriptions array with \n (new line)
+        caseEmbed.setDescription(`\`\`\`${caseDescriptions.join('\n')}\`\`\``);
+      }
+
+      // Send the leaderboard embed
+      await ctx.interaction.editReply({ embeds: [caseEmbed] });
+   }
+
+   if (sortType === 'Type') {
+    const caseData = await caseSchema.find({ Guild: guild.id })
+    .sort({
+      Type: -1,
+    });
+
+    // Create an embed to send the leaderboard
+    const caseEmbed = new EmbedBuilder()
+      .setTitle(`Moderation: Cases (Sort: User)`)
+      .setColor(0x8269c2)
+      .setFooter({
+        text: `${ctx.guild.name}`, // Footer text
+        iconURL: ctx.guild.iconURL()
+      })
+      .setTimestamp();
+
+    // If no leaderboard, inform user.
+    if (!caseData.length) {
+      caseEmbed.setDescription('There are no cases to show.');
+    } else {
+      // Array consisting of name/level/xp details (string) for each user
+      const caseDescriptions = [];
+
+      // Iterates through each xpData document to create name/level/xp details for user and pushes them to
+      // to userDescriptions array
+      for (caseDataDoc of caseData) {
+        const { _id, User, Type, Moderator } = caseDataDoc;
+        const member = await client.users.fetch(User);
+        const mod = await client.users.fetch(Moderator);
+        caseDescriptions.push(`Case: ${_id} | User: ${member.tag} | Type: ${Type} | Moderator: ${mod.tag}`);
+      };
+
+      // Join elements in user descriptions array with \n (new line)
+      caseEmbed.setDescription(`\`\`\`${caseDescriptions.join('\n')}\`\`\``);
+    }
+
+    // Send the leaderboard embed
+    await ctx.interaction.editReply({ embeds: [caseEmbed] });
+   }
+
+   if (sortType === 'mod') {
+    const caseData = await caseSchema.find({ Guild: guild.id })
+    .sort({
+      Moderator: -1,
+    });
+
+    // Create an embed to send the leaderboard
+    const caseEmbed = new EmbedBuilder()
+      .setTitle(`Moderation: Cases (Sort: Moderator)`)
+      .setColor(0x8269c2)
+      .setFooter({
+        text: `${ctx.guild.name}`, // Footer text
+        iconURL: ctx.guild.iconURL()
+      })
+      .setTimestamp();
+
+    // If no leaderboard, inform user.
+    if (!caseData.length) {
+      caseEmbed.setDescription('There are no cases to show.');
+    } else {
+      // Array consisting of name/level/xp details (string) for each user
+      const caseDescriptions = [];
+
+      // Iterates through each xpData document to create name/level/xp details for user and pushes them to
+      // to userDescriptions array
+      for (caseDataDoc of caseData) {
+        const { _id, User, Type, Moderator } = caseDataDoc;
+        const member = await client.users.fetch(User);
+        const mod = await client.users.fetch(Moderator);
+        caseDescriptions.push(`Case: ${_id} | User: ${member.tag} | Type: ${Type} | Moderator: ${mod.tag}`);
+      };
+
+      // Join elements in user descriptions array with \n (new line)
+      xpEmbed.setDescription(`\`\`\`${caseDescriptions.join('\n')}\`\`\``);
+    }
+
+    // Send the leaderboard embed
+    await ctx.interaction.editReply({ embeds: [caseEmbed] });
+   }
   }
 })
